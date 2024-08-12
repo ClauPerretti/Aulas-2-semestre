051d66341028c02ec2441ceaaa32578ae1885b95
ClauPerretti <claudio.perretti@fatec.sp.gov.br>
2024-08-12 18:36:55 -0300

    criação de pastas de materias

--- exercicio2.ts
+++
@@ -1,9 +0,0 @@
-function converter(vet:Array<number>){
-    const res:Array<string> = [];
-    for(let i = 0; i < vet.length; i++){
-        res[i] = "" + vet[i];
-    }
-    return res;
-}
-const vetor = [5,3,1,8,2];
-console.log("Resultado:", converter(vetor))
